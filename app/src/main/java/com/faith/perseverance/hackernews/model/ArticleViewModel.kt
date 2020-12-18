d5f8dfc46bc7eeb0fff22a769de00e19f656690d
chrisdholmes1 <chrisdholmes1@gmail.com>
2020-12-17 16:09:08 -0800

    Updated ArticleModel - removed unecessary variables

--- app/src/main/java/com/faith/perseverance/hackernews/model/ArticleViewModel.kt
+++ app/src/main/java/com/faith/perseverance/hackernews/model/ArticleViewModel.kt
@@ -3,7 +3,6 @@ package com.faith.perseverance.hackernews.model
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -17,10 +16,6 @@ class ArticleViewModel(): ViewModel() {
         MutableLiveData<List<Article>>()
     }
 
-    private val _navigateToWebFragment = MutableLiveData<Boolean?>()
-
-    val navigativeToWebFragment: LiveData<Boolean?> get() = _navigateToWebFragment
-
 
     private var TAG: String = "ArticleViewModel"
 
@@ -28,18 +23,6 @@ class ArticleViewModel(): ViewModel() {
 
         getArticles()
     }
-
-    fun doneNavigating()
-    {
-        _navigateToWebFragment.value = null
-    }
-
-    fun startNavigating()
-    {
-        _navigateToWebFragment.value = true
-    }
-
-
 
     @RequiresApi(Build.VERSION_CODES.O)
     fun getArticles()
